94d96a9a70467d4e63260e75d0acd79c319b6035
brycx <brycx@protonmail.com>
2021-09-28 10:08:59 +0200

    typedefs: Implement Drop for construct_tag

--- src/typedefs.rs
+++ src/typedefs.rs
@@ -611,6 +611,7 @@ macro_rules! construct_tag {
         }
 
         impl_omitted_debug_trait!($name);
+        impl_drop_trait!($name);
         impl_ct_partialeq_trait!($name, unprotected_as_bytes);
 
         impl $name {
